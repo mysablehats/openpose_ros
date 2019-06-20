ab6881868939c031e02e7f5c22b076ae7e6e2c68
fred <frederico.klein@plymouth.ac.uk>
2019-06-20 11:12:25 +0100

    improved matching by resolving similarities of skeletons per joint

--- reorderskeleton.py
+++ reorderskeleton.py
@@ -45,26 +45,38 @@ def genAU(N):
         AU.append(deepcopy(apoint))
     return AU
 
-def rematchcentroids(SK,A,U):
+def rematchcentroids(SKA,SKU):
+    PERJOINT = True
     ### first let's remove the elemetary solutions:
-    if (len(U) ==1 and len(A) ==1) or len(U) == 0:
+    if (len(SKU) ==1 and len(SKA) ==1) or len(SKU) == 0:
         ##nothing to be matched
-        return SK
+        return SKU
     rospy.logdebug('something to match!')
-    assert len(SK) == len(U) ### they need to be the same length or I won't be able to match them!
-    newsk = [[] for i in range(max([len(A),len(U)]))] ## hmmm this will continue to grow?
-    mydistmat = 500*np.ones([len(A),len(U)]) ### actually should be 'infinity' or a large number
-    for i,Ai in enumerate(A):
-        for j,Uj in enumerate(U):
+    # assert len(SK) == len(U) ### they need to be the same length or I won't be able to match them!
+    newsk = [[] for i in range(max([len(SKA),len(SKU)]))] ## hmmm this will continue to grow?
+    mydistmat = 500*np.ones([len(SKA),len(SKU)]) ### actually should be 'infinity' or a large number
+    for i,Ai in enumerate(SKA):
+        for j,Uj in enumerate(SKU):
             ### if I ever put depth..
-            mydistmat[i,j] = math.sqrt((Ai.x - Uj.x)*(Ai.x - Uj.x) + (Ai.y - Uj.y)*(Ai.y - Uj.y) + (Ai.z - Uj.z)*(Ai.z - Uj.z))
+            ####Now this distance is the distance from every joint. I've done this before. There is also the 3N euclidean distance option, which is maybe better. should test both!
+            mydistmat[i,j] = 0
 
+            for iPersonJointk, jPersonJointk in zip(Ai,Uj):
+                if PERJOINT:
+                    mydistmat[i,j] += math.sqrt((iPersonJointk.x - jPersonJointk.x)*(iPersonJointk.x - jPersonJointk.x) + (iPersonJointk.y - jPersonJointk.y)*(iPersonJointk.y - jPersonJointk.y) + (iPersonJointk.z - jPersonJointk.z)*(iPersonJointk.z - jPersonJointk.z))
+                else:
+                    mydistmat[i,j] += ((iPersonJointk.x - jPersonJointk.x)*(iPersonJointk.x - jPersonJointk.x) + (iPersonJointk.y - jPersonJointk.y)*(iPersonJointk.y - jPersonJointk.y) + (iPersonJointk.z - jPersonJointk.z)*(iPersonJointk.z - jPersonJointk.z))
+            # mydistmat[i,j] = math.sqrt((Ai.x - Uj.x)*(Ai.x - Uj.x) + (Ai.y - Uj.y)*(Ai.y - Uj.y) + (Ai.z - Uj.z)*(Ai.z - Uj.z))
+            if PERJOINT:
+                pass ## computations are done
+            else:
+                mydistmat[i,j] = math.sqrt(mydistmat[i,j])
     ### maybe there should be also some distance threshold so that I don't try to match really distant people. afff...
-    if len(A)>len(U):
+    if len(SKA)>len(SKU):
         ##some people will disappear!
         rospy.logdebug('A is bigger than U. People will disappear!')
 
-    elif len(U)>len(A):
+    elif len(SKU)>len(SKA):
         ##some people will disappear!
         rospy.logdebug('U is bigger than A. New people will appear!')
 
@@ -72,8 +84,8 @@ def rematchcentroids(SK,A,U):
     else: ## len(U) == len(A):
         pass
         #all is good? or new people should appear and some people should disappear???
-    inda = range(len(A))
-    indu = range(len(U))
+    inda = range(len(SKA))
+    indu = range(len(SKU))
     while(mydistmat.shape[0]>0 and mydistmat.shape[1]>0 ):
         print(mydistmat)
         lin,col = ind = np.unravel_index(np.argmin(mydistmat, axis=None), mydistmat.shape)
@@ -82,8 +94,8 @@ def rematchcentroids(SK,A,U):
         # col = mydistmat.min(0).argmin() ## this is the closest match so far
         realindlin = inda[lin]
         realindcol = indu[col]
-        print("matching A: %d to U:%d "%(realindlin,realindcol))
-        newsk[realindlin] = SK[realindcol]
+        print("matching SKA: %d to SKU:%d "%(realindlin,realindcol))
+        newsk[realindlin] = SKU[realindcol]
         #print(newsk)
         ## SK.pop(col)
         ### now delete the col and lin
@@ -98,31 +110,27 @@ def rematchcentroids(SK,A,U):
         print(mydistmat.shape)
     ### i think i need to add all of the indu indexes remaining...
     print('remaining indexes %s'%indu)
-    newsk.extend([SK[i] for i in indu ])
+    newsk.extend([SKU[i] for i in indu ])
     return filter(None, newsk) #, mydistmat
     # print newsk
     # return mydistmat
     # return SK
 
 class ShowSkel:
-    def __init__(self,polyOrArr):
+    def __init__(self):
         rospy.init_node('showskel_node', anonymous=True, log_level=rospy.DEBUG)
         image_topic = 'im_in'# rospy.get_param('~image_topic','camera/rgb/image_color')
         rospy.loginfo('Input image topic being read as background image: {}'.format(image_topic))
         ## Acentroids are the previous known centroids for skeletons I found already. Ucentroids are the new from current frame that I need to match with the old ones.
-        self.Acentroids = []
-        self.Ucentroids = []
+        # self.Acentroids = []
+        # self.Ucentroids = []
+        self.SKA = []
+        self.SKU = []
+
         self.confidence_treshold = 0.5
-        if polyOrArr == 'poly':
-            self.hss =  rospy.Subscriber('my_topic', Polygon, self.updateskelPoly)
-            self.SK = []
-            Polygon()
-            self.SK.append(SK.points) ### this is just a list. i could initialize it to an empty list...
-        elif polyOrArr == 'array':
-            self.hss =  rospy.Subscriber('skel', Float32MultiArray, self.updateskelArray)
-            self.SK = []
+
+        self.hss =  rospy.Subscriber('skel', Float32MultiArray, self.updateskelArray)
         self.cvbridge = CvBridge()
-        #maybe this is a good time for a wait_for_message; to initialize this thing correctly
         rospy.logdebug('waiting for image topic to publish first image so I can initialize this guy ')
         self.bg = self.cvbridge.imgmsg_to_cv2( rospy.wait_for_message(image_topic, Image), 'bgr8')
         self.bgstack = []
@@ -133,7 +141,6 @@ class ShowSkel:
         self.rois = rospy.Publisher('rois', PersonArray, queue_size=1)
         self.mainskelcolor = (0,255,0)
         self.otherskelscolor = (255,0,0)
-        # self.bg = np.array([],'uint8')
         self.font = cv2.FONT_HERSHEY_SIMPLEX
 
         rospy.loginfo('ShowSkel initialized. ')
@@ -158,10 +165,6 @@ class ShowSkel:
         self.bg = self.bgstack[0]
         # self.bg = self.cvbridge.imgmsg_to_cv2(data, 'bgr8')
 
-    def updateskelPoly(self,data):
-        # rospy.logdebug('updateskelPoly called')
-        self.SK = data.points
-
     def updateskelArray(self,data):
         # rospy.logdebug('updateskelArray called')
         ###not flexible. needs to have person, bodyparts and xyconfidence:
@@ -183,17 +186,16 @@ class ShowSkel:
                 # rospy.logdebug(type(self.xyconfidence))
                 assert self.xyconfidence == 3 ##it has to be 3.
 
-        self.SK = []
-        self.Acentroids = self.Ucentroids
-        self.Ucentroids = []
+        # self.SKA = self.SKU
+        self.SKU = []
         debugnumpeople = 0
         for Nperson in range(self.numpersons):
             personchunkstart =   int(Nperson*self.personstride)
             # rospy.logdebug(personchunkstart)
             thisPersonData = data.data[personchunkstart:personchunkstart+self.personstride]
             thisPerson = []
-            thisCentroid = MyPoint()
-            thisCentroid.zeroit()
+            # thisCentroid = MyPoint()
+            # thisCentroid.zeroit()
             for bodypart in range(self.numbodyparts):
                 thisBodypart = MyPoint()
                 thisBodypart.x =            thisPersonData[bodypart*self.xyconfidence]
@@ -201,31 +203,31 @@ class ShowSkel:
                 thisBodypart.confidence =   thisPersonData[bodypart*self.xyconfidence+2]
                 thisPerson.append(thisBodypart)
                 ###calculating the centroid:
-                if thisBodypart.confidence > self.confidence_treshold: ### will try avoiding using bad bodyparts to compute centroid
-                    thisCentroid.x += thisBodypart.x
-                    thisCentroid.y += thisBodypart.y
-                    thisCentroid.z += thisBodypart.z
-                    thisCentroid.confidence += thisBodypart.confidence ### does this make sense?
-            thisCentroid.x = thisCentroid.x/self.numbodyparts
-            thisCentroid.y = thisCentroid.y/self.numbodyparts
-            thisCentroid.z = thisCentroid.z/self.numbodyparts
-            thisCentroid.confidence = thisCentroid.confidence/self.numbodyparts
-            self.SK.append(deepcopy(thisPerson))
-            self.Ucentroids.append(deepcopy(thisCentroid))
+                # if thisBodypart.confidence > self.confidence_treshold: ### will try avoiding using bad bodyparts to compute centroid
+                    # thisCentroid.x += thisBodypart.x
+                    # thisCentroid.y += thisBodypart.y
+                    # thisCentroid.z += thisBodypart.z
+                    # thisCentroid.confidence += thisBodypart.confidence ### does this make sense?
+            # thisCentroid.x = thisCentroid.x/self.numbodyparts
+            # thisCentroid.y = thisCentroid.y/self.numbodyparts
+            # thisCentroid.z = thisCentroid.z/self.numbodyparts
+            # thisCentroid.confidence = thisCentroid.confidence/self.numbodyparts
+            self.SKU.append(deepcopy(thisPerson))
+            # self.Ucentroids.append(deepcopy(thisCentroid))
             debugnumpeople +=1
             rospy.logdebug('added a person %d'%debugnumpeople)
         ### last but not the least, I need to reorder SK to match the centroids
-        self.SK = rematchcentroids(self.SK,self.Acentroids,self.Ucentroids)
+        self.SKA = rematchcentroids(self.SKA,self.SKU)
         # rospy.loginfo(self.SK)
     def pubimage(self):
         # rospy.logdebug('pubinfo called')
         debugnumpeople = 0
         #print(type(self.bg    ))
         firstskel = True
-        if self.SK:
+        if self.SKA:
             personarray_msg = PersonArray()
             personarray_msg.header = Header()
-            for per_index,persons in enumerate(self.SK):
+            for per_index,persons in enumerate(self.SKA):
                 if firstskel:
                     skelcolor = self.mainskelcolor
                 #     skeltext = '0'
@@ -262,5 +264,5 @@ class ShowSkel:
 
 if __name__ == '__main__':
 
-    sf = ShowSkel('array')
+    sf = ShowSkel()
     sf.run()
